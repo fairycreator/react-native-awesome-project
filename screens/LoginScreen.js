0eeedf27bb88dc38845f95bf4f6e92236801231d
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-12-01 21:37:04 +0100

    login updated

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,218 +1,157 @@
 import React, { useState } from "react";
 import {
   View,
+  Text,
   TextInput,
   TouchableOpacity,
-  Text,
   StyleSheet,
+  StatusBar,
+  SafeAreaView,
   ImageBackground,
 } from "react-native";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
 
-  const handleLogin = () => {};
+  const handleLogin = () => {
+    console.log("Email:", email, "Password:", password);
+  };
 
-  const navigateToRegister = () => {};
+  const navigateToRegister = () => {
+    console.log("Navigate to registration screen");
+  };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
       <ImageBackground
-        style={styles.absoluteFill}
         source={require("../assets/background_alps.jpeg")}
+        style={styles.absoluteFill}
       >
-        {/* White panel with rounded top corners */}
-        <View style={styles.whitePanel} />
-        {/* The header bar, which could be a status bar */}
-        <View style={styles.headerBar}>
-          {/* Simulate the notch design */}
-          <View style={styles.notch} />
-        </View>
-        {/* Title "Увійти" */}
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>Увійти</Text>
-        </View>
-        {/* Email input field */}
-        <TextInput style={styles.emailInputContainer}>
-          <Text style={styles.emailPlaceholder}>Адреса електронної пошти</Text>
-        </TextInput>
-        {/* Password input field with "Показати" text */}
-        <TextInput style={styles.passwordInputContainer}>
-          <Text style={styles.passwordDots}>••••••••••••</Text>
-          <Text style={styles.showText}>Показати</Text>
-        </TextInput>
-        {/* Login button */}
-        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-          <Text style={styles.loginButtonText}>Увійти</Text>
-        </TouchableOpacity>
-        {/* Registration prompt */}
-        <View style={styles.registrationPrompt}>
-          <Text style={styles.registrationText}>Немає акаунту? </Text>
-          <TouchableOpacity onPress={navigateToRegister}>
-            <Text style={[styles.registrationText, styles.registrationLink]}>
+        <View style={styles.whitePanel}>
+          <Text style={styles.title}>Увійти</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Адреса електронної пошти"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Пароль"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={showPassword}
+            />
+            <TouchableOpacity
+              style={styles.showPasswordButton}
+              onPress={() => setShowPassword(!showPassword)}
+            >
+              <Text style={styles.showPasswordText}>
+                {showPassword ? "Показати" : "Сховати"}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Увійти</Text>
+          </TouchableOpacity>
+          <View style={styles.registerContainer}>
+            <Text style={styles.registerText}>Немає акаунту?</Text>
+            <Text
+              style={[styles.registerText, styles.registerLink]}
+              onPress={navigateToRegister}
+            >
               Зареєструватися
             </Text>
-          </TouchableOpacity>
-        </View>
-        {/* Bottom swipe-up bar */}
-        <View style={styles.bottomBar}>
-          <View style={styles.swipeIndicator} />
+          </View>
         </View>
       </ImageBackground>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   absoluteFill: {
     flex: 1,
-    width: null,
-    height: null,
     resizeMode: "cover",
   },
-  container: {
-    width: "100%",
-    height: "100%",
-    position: "relative",
-    backgroundColor: "white",
-  },
-  // absoluteFill: {
-  //   width: 375,
-  //   height: 812,
-  //   position: "absolute",
-  // },
   whitePanel: {
     position: "absolute",
-    top: "50%",
-    width: "100%",
-    height: "50%",
+    top: "40%",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "white",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    backgroundColor: "white",
-  },
-  headerBar: {
-    width: 375,
-    height: 44,
-    position: "absolute",
-    flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
+    paddingTop: 30,
   },
-  notch: {
-    width: 219,
-    height: 30,
-    backgroundColor: "black",
-    position: "absolute",
-    top: 0,
-    left: 78,
-  },
-  titleContainer: {
-    position: "absolute",
-    width: 343,
-    top: 355,
-    left: 16,
-    alignItems: "center",
-  },
-  titleText: {
-    textAlign: "center",
+  title: {
     color: "#212121",
     fontSize: 30,
     fontFamily: "Roboto",
     fontWeight: "500",
-    marginTop: 60,
+    letterSpacing: 0.3,
+    marginBottom: 30,
   },
-  emailInputContainer: {
-    position: "absolute",
-    width: 343,
-    height: 50,
-    top: 423,
-    left: 16,
+  inputContainer: {
+    width: "90%",
     backgroundColor: "#F6F6F6",
     borderRadius: 8,
-    borderWidth: 1,
     borderColor: "#E8E8E8",
-    justifyContent: "center",
-    paddingLeft: 16,
+    borderWidth: 1,
+    marginBottom: 16,
+    flexDirection: "row",
+    alignItems: "center",
   },
-  emailPlaceholder: {
-    color: "#BDBDBD",
+  input: {
+    flex: 1,
+    padding: 15,
     fontSize: 16,
-    fontFamily: "Roboto",
-    fontWeight: "400",
+    color: "#BDBDBD",
   },
-  passwordInputContainer: {
-    position: "absolute",
-    width: 343,
-    height: 50,
-    top: 489,
-    left: 16,
-    backgroundColor: "#F6F6F6",
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: "#E8E8E8",
-    justifyContent: "center",
-    paddingLeft: 16,
+  showPasswordButton: {
+    paddingRight: 15,
   },
-  passwordDots: {
-    color: "#212121",
-    fontSize: 15,
-    fontFamily: "Roboto",
-    fontWeight: "400",
-  },
-  showText: {
-    position: "absolute",
-    right: 16,
-    top: 16,
+  showPasswordText: {
     color: "#1B4371",
     fontSize: 16,
-    fontFamily: "Roboto",
-    fontWeight: "400",
   },
-  loginButton: {
-    position: "absolute",
-    left: 16,
-    top: 582,
-    width: 343,
-    height: 51,
+  button: {
+    width: "90%",
     backgroundColor: "#FF6C00",
+    padding: 20,
     borderRadius: 100,
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 20,
+    marginTop: 43,
   },
-  loginButtonText: {
+  buttonText: {
     color: "white",
     fontSize: 16,
-    fontFamily: "Roboto",
-    fontWeight: "400",
   },
-  registrationPrompt: {
-    position: "absolute",
-    top: 649,
-    left: 62,
+  registerContainer: {
     flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 20,
   },
-  registrationText: {
+  registerText: {
     color: "#1B4371",
     fontSize: 16,
-    fontFamily: "Roboto",
-    fontWeight: "400",
   },
-  registrationLink: {
+  registerLink: {
     textDecorationLine: "underline",
-  },
-  bottomBar: {
-    position: "absolute",
-    width: "100%",
-    top: 779,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  swipeIndicator: {
-    width: 134,
-    height: 5,
-    backgroundColor: "#212121",
-    borderRadius: 100,
   },
 });
 
