4dd4902682e10ad324bfe6a0e5f1c6e2ba3b8b22
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-12-04 21:25:41 +0100

    app updated

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
